659c96618984f95ced740d60bbcab0f228423f32
hermanwongkm <herman_wong_km@outlook.com>
2022-02-13 19:41:05 +0800

    [Feat] Add data manipulation for forming entry'

--- src/db/schemas/foodMap/index.js
+++ src/db/schemas/foodMap/index.js
@@ -13,7 +13,17 @@ const FoodMapLocationType = new GraphQLObjectType({
   description: 'this represents a food map location',
   fields: () => {
     return {
+      entry: {type: locationDetails},
       id: { type: GraphQLInt },
+    };
+  },
+});
+
+const locationDetails = new GraphQLObjectType({
+  name: 'location_details',
+  description: 'this represents a a location details',
+  fields: () => {
+    return {
       title: { type: GraphQLString },
       description: { type: GraphQLString },
       address: { type: GraphQLString },
@@ -26,10 +36,16 @@ const FoodMapLocationsSchema = {
   type: new GraphQLList(FoodMapLocationType),
   args: {},
   async resolve(root, args) {
-    const locations = await FoodMapLocation.findAll();
-    if (locations) {
-      return locations;
-    }
+    const locations = await FoodMapLocation.findAll({
+      raw: true, // Sequelize wraps all it's return values in a virtual object that contains meta data. If you have an object and you just want the undecorated data value
+      nest : true // if you have associations with include that gets flattened. So, we can use another parameter nest:true 
+    });
+    const parsedLocations = locations.map(location => {
+      // assigns remaining keys to a new `entry` object using destructuring
+      const {id, ...entry} = location
+      return {id, entry}
+    });
+      return parsedLocations;
   }
 }
 
